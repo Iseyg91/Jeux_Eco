9372587a0304ffdee5ae5ba90db04bde67db77cc
Iseyg91 <gokalpahmet74@gmail.com>
2025-04-26 11:46:36 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -3023,8 +3023,17 @@ async def russianroulette(ctx, arg: str):
             color=discord.Color.from_rgb(255, 92, 92)
         ))
 
-from discord.ext import commands
-from discord import app_commands
+
+active_roulette_players = set()
+
+# Numéros corrigés
+RED_NUMBERS = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36]
+BLACK_NUMBERS = [2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35]
+EVEN_NUMBERS = [i for i in range(2, 37, 2)]
+ODD_NUMBERS = [i for i in range(1, 37, 2)]
+COLUMN_1 = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
+COLUMN_2 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
+COLUMN_3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
 
 # Options possibles pour space
 SPACES = (
